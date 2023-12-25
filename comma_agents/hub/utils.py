98856a5ce1be9b100c2bd76e8b07166f1d848ba2
Your Name <you@example.com>
2023-12-24 23:44:58 -0600

    Well... That was a few hours of trying to auto import remote repo magic...

---
+++ comma_agents/hub/utils.py
@@ -0,0 +1,4 @@
+import os
+
+REPO_LINK = "https://github.com/CloAI/CommaAgentsHub.git"
+COMMA_AGENTS_HUB_DIRECTORY = os.path.expanduser("~/.comma_agents/hub")
--- examples/hub_test.py
+++ examples/hub_test.py
@@ -1,1 +1,7 @@
-from comma_agents.hub.agents.nateageek import OpenAIAPIAgent
+import comma_agents.hub.agents.nateageek as nateageek_agents
+
+agent = nateageek_agents.openai_agent.openai_api_agent.OpenAIAPIAgent("Open AI Agent", config={
+    "model_name": "gpt4",
+})
+
+print(agent.call("Hello, how are you?"))
