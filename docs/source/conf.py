385935d45192b8d03ceee41c8cfa806d3e6e202f
Your Name <you@example.com>
2024-08-21 18:23:42 -0500

    Version bump

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,7 @@ sys.path.insert(0, os.path.abspath("../"))
 project = 'Comma Agents'
 copyright = '2023, Nathaniel Hatch-Martinez'
 author = 'Nathaniel Hatch-Martinez'
-release = '0.0.6'
+release = '0.0.7'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
